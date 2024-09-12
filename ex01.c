03e4b46fbb946a2e3e20d55d6eabefcd220ec361
Thiago Alves <thiagoalvs10@gmail.com>
2024-09-12 10:39:53 -0300

    hjkhj

---
+++ ex01.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int i;
+    float nota;
+
+    for (i = 1; i <= 20; i++) {
+    printf("Digite a nota do %do aluno: ", i);
+    scanf("%f", &nota);
+    }
+
+
+
+    return 0;
+}
